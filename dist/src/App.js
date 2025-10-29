0191e5c4b5838d76654a04b804422b46a3369496
iftekhar moinuddin <134924046+moinnuddinnn@users.noreply.github.com>
2025-10-29 17:08:33 +0530

    Delete dist/src/App.js

--- dist/src/App.js
+++
@@ -1,6 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-//import "./types/shims";
-import Editor from "./components/Editor";
-export default function App() {
-    return (_jsx("div", { style: { height: "100vh", background: "#1e1e1e", color: "white" }, children: _jsx(Editor, {}) }));
-}
