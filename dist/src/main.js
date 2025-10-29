038c3a9367e80b2051b4c1f06180b2ddde2697c0
iftekhar moinuddin <134924046+moinnuddinnn@users.noreply.github.com>
2025-10-29 17:08:44 +0530

    Delete dist/src/main.js

--- dist/src/main.js
+++
@@ -1,5 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./styles/global.css";
-ReactDOM.createRoot(document.getElementById("root")).render(_jsx(App, {}));
